89b2f7e09f93ae775dae7ed3d7fa1013896c38be
wenlong <liwenlong@winwin-inc.com>
2020-12-16 17:38:18 +0800

    mod: adjust ide-helper config

--- config/ide-helper.php
+++ config/ide-helper.php
@@ -58,7 +58,7 @@ return [
     |
     */
 
-    'write_model_magic_where' => true,
+    'write_model_magic_where' => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'write_model_relation_count_properties' => true,
+    'write_model_relation_count_properties' => false,
 
     /*
     |--------------------------------------------------------------------------
