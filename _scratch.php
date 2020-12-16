4c5b19dad9e807a5f3cba18217fb1cb8626730ab
wenlong <liwenlong@winwin-inc.com>
2020-12-16 13:15:16 +0800

    add: _scratch.php file

---
+++ _scratch.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+$app->make(\App\Console\Kernel::class)->bootstrap();
+
+/** ============== Write Codes Below ================= **/
+
+echo 'Start Laravel from CLI';
