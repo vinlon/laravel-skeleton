1e1bc97a0adf9ff1f983f9ce660880ec5ce7423c
liwenlong <vinlon1009@gmail.com>
2021-01-01 23:02:36 +0800

    mod: BaseModel改为抽象类，默认将除id, crated_at, updated_at外的其它字段设为fillable

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -4,7 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class BaseModel extends Model
+abstract class BaseModel extends Model
 {
     use SerializeDateTrait;
+
+    // 除 id, created_at, updated_at 外的其它字段都是 fillable
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 }
